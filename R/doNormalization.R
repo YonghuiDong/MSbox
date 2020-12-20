1ad7e76d98df141e28bd83d8064244ad0756fa05
YonghuiDong <yonghui.dong@gmail.com>
2020-12-21 00:31:32 +0200

    Update doNormalization.R
    add quantile normalization

--- R/doNormalization.R
+++ R/doNormalization.R
@@ -2,8 +2,7 @@
 #' @description perform normalization
 #' @param x sample ion intensity matrix
 #' @param method normalization method: (1) LBME: linear baseline normalization based on mean values;
-#' (2) LBMD: linear baseline normalization based on median values; (3) PQN: probabilistic quotient normalization;
-#' (4): MEDIAN: median normalization; (5) TIC: TIC normalization
+#' (2) LBMD: linear baseline normalization based on median values; (3) PQN: probabilistic quotient normalization.
 #' @return normalized data matrix
 #' @export
 #' @examples
@@ -13,14 +12,15 @@
 doNormalization <-function(x, method = NULL){
   #(1) check input
   if(is.null(method)) {stop("Please select a normalization method")}
-  if (!(toupper(method) %in% c("LBME", "LBMD", "PQN", "MEDIAN", "TIC")))
+  if (!(toupper(method) %in% c("LBME", "LBMD", "PQN", "QT")))
   {stop("Invalid normalization method")}
+  x[x == 0] <- 1 # make sure that data do not contain any zeros
+  x = t(x) # feature in row and sample in column
 
-  x = t(x)
   #(2)perform normalization
   ##(2.1) linear baseline normalization based on mean values
   if(toupper(method) =="LBME"){
-    linear.baseline <- apply(x, 1, median) #compute baseline
+    linear.baseline <- apply(x, 1, function(x) median(x, na.rm = T)) #compute baseline
     baseline.mean <- mean(linear.baseline)
     sample.means <- apply(x, 2, mean)
     linear.scaling <- baseline.mean/sample.means
@@ -44,16 +44,17 @@ doNormalization <-function(x, method = NULL){
     norm.metabo.data <-t(t(x)/quotient.median)
   }
 
-  ##(2.4) Median
-  if(toupper(method) == "MEDIAN"){
-    norm_vector <- apply(x, 1, median)
-    norm.metabo.data <- x / norm_vector
-  }
+  #(2.4) QT
+  if(toupper(method) == "QT"){
+    df_rank <- apply(x, 2, rank, ties.method = "min")
+    df_sorted <- data.frame(apply(x, 2, sort))
+    df_mean <- apply(df_sorted, 1, mean)
 
-  ##(2.5) TIC
-  if(toupper(method) == "TIC"){
-    norm_vector <- rowSums(x)
-    norm.metabo.data <- x / norm_vector
+    index_to_mean <- function(my_index, my_mean){
+      return(my_mean[my_index])
+    }
+    norm.metabo.data <- apply(df_rank, 2, index_to_mean, my_mean=df_mean)
+    rownames(norm.metabo.data) <- rownames(x)
   }
 
   return(t(norm.metabo.data))
