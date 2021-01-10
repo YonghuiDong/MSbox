88f1c54fec99b3ba126fb612fbf24a18056e7e91
Dong <yonghui.dong@gmail.com>
2021-01-10 22:48:18 +0200

    update viewTIC

--- R/viewTIC.R
+++ R/viewTIC.R
@@ -1,9 +1,10 @@
-#' @title view TIC
-#' @description view variations of TIC among samples
+#' @title View TIC
+#' @description View variations of TIC among samples
 #' @param x sample ion intensity matrix,row sample, column feature.
 #' @param Batch sample batch information. If missing, all the samples will be considered from the same batch
 #' @param Seq sample sequence with each batch. If missing, the Seq will be automatically assigned according to sample order
 #' @param Group sample group information
+#' @param resultBy show the result by Batch or by Group (default).
 #' @param Trans How should data be transformed, "LOG2", "LOG10", or NULL transformation?
 #' @importFrom reshape2 melt
 #' @importFrom utils globalVariables
@@ -11,19 +12,20 @@
 #' @return a box plot
 #' @export
 #' @examples
-#' dat <- matrix(runif(100*9), ncol = 100, nrow = 10)
-#' myGroup <- rep_len(LETTERS[1:5], 10)
-#' mySeq <- c(1:10)
-#' out <- viewTIC(dat, Group = myGroup)
+#' dat <- matrix(runif(100*9), ncol = 100, nrow = 27)
+#' myGroup <- rep_len(LETTERS[1:3], 27)
+#' myBatch <- rep(1:3, each = 9, times = 1)
+#' mySeq <- c(1:27)
+#' out <- viewTIC(dat, Group = myGroup, Batch = myBatch, resultBy = "Batch")
 
-viewTIC <- function(x, Group = NULL, Batch = NULL, Seq = NULL, Trans = "none"){
+viewTIC <- function(x, Seq = NULL, Batch = NULL, Group = NULL, Trans = "none", resultBy = "Group"){
 
    #(1) check input
   Group <- as.factor(Group)
   if(is.null(Group)){stop("Please include group information")}
-  if(is.null(Batch)){Batch = 1}
   if(is.null(Seq)){Seq = 1:nrow(x)}
   if(length(Group) != nrow(x)){stop("Missing group informaiton detected")}
+  if(!(resultBy %in% c("Group", "Batch"))){stop("Please choose if you want to show the result by Batch or by Group?")}
 
   ##(2) transform
   x[x == 0] <- 1
@@ -32,20 +34,35 @@ viewTIC <- function(x, Group = NULL, Batch = NULL, Seq = NULL, Trans = "none"){
 
   #(3) view TIC
   ## order the data
-  datOrder <- paste("B", Batch, "S", Seq, "G", Group, sep = "")
-  dat2 <- cbind.data.frame(Order = datOrder, x)
-  dat2 <- dat2[order(dat2$Order), ]
-  ## reshape data
-  dat3 <- suppressMessages({reshape2::melt(dat2)})
-  dat3 <- dat3[, -2]
-  colnames(dat3) <- c("Sample", "Intensity")
+  if(is.null(Batch)) {
+    dat2 <- cbind.data.frame(Seq = Seq, Group = Group, x)
+    dat2 <- dat2[order(dat2$Seq, dat2$Group), ]
+    dat3 <- melt(dat2, id = c("Seq", "Group"))
+  } else {
+    dat2 <- cbind.data.frame(Seq = Seq, Batch = Batch, Group = Group, x)
+    dat2 <- dat2[order(dat2$Seq, dat2$Batch, dat2$Group), ]
+    dat3 <- melt(dat2, id = c("Seq", "Batch", "Group"))
+  }
+
   ## to plot
-  p <- ggplot2::ggplot(dat3, aes(x = Sample, y = Intensity, color = Sample)) +
-    geom_boxplot() +
-    theme_bw() +
-    xlab("Sample (in Batch-Seq-Group order)") +
-    theme(legend.position = "none",
-          axis.text.x = element_text(angle = 90, hjust = 1))
+  dat3$Group <- as.factor(dat3$Group)
+  dat3$Seq <- as.factor(dat3$Seq)
+  if(resultBy == "Group"){
+    p <- ggplot2::ggplot(dat3, aes(x = Seq, y = value, fill = Group)) +
+      geom_boxplot(alpha = 0.5) +
+      facet_grid(. ~ Group, scales = "free_x", space = "free_x") +
+      theme_bw() +
+      xlab("Sample (injection sequence)") +
+      ylab("Intensity")
+  } else if (resultBy == "Batch"){
+    dat3$Batch <- as.factor(dat3$Batch)
+    p <- ggplot2::ggplot(dat3, aes(x = Seq, y = value, fill = Batch)) +
+      geom_boxplot(alpha = 0.5) +
+      facet_grid(. ~ Batch, scales = "free_x", space = "free_x") +
+      theme_bw() +
+      xlab("Sample (injection sequence)") +
+      ylab("Intensity")
+    }
 
   return(p)
 }
