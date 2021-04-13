78f1642b9d7e07f4777ff0f198915997bddd0e6a
Dong <yonghui.dong@gmail.com>
2021-04-13 23:32:30 +0300

    Update searchDB.R
    update searchDB()

--- R/searchDB.R
+++ R/searchDB.R
@@ -16,34 +16,39 @@
 searchDB <- function(DF, DB, ppm = 5, RT = 0.2, useRT = FALSE){
 
   #(1) input check
-  if(!'mz' %in% colnames(DF)) {stop("Column 'mz' is not found in your data")}
-  if(!'mz' %in% colnames(DB)) {stop("Column 'mz' is not found in your database")}
+  ## rename DF and DB
+  names(DF)[names(DF) == "mz"] <- "My.mz"
+  names(DF)[names(DF) == "rt"] <- "My.RT"
+  names(DB)[names(DB) == "mz"] <- "DB.mz"
+  names(DB)[names(DB) == "rt"] <- "DB.RT"
+  if(!'My.mz' %in% colnames(DF)) {stop("Column 'mz' is not found in your data")}
+  if(!'DB.mz' %in% colnames(DB)) {stop("Column 'mz' is not found in your database")}
   cat("Searching started...\n")
 
   #(2) define function and search
   expand.grid.df <- function(...) Reduce(function(...) merge(..., by = NULL), list(...))
-  Result <- vector(mode = "list", length = length(DF$mz))
-  pb <- txtProgressBar(min = 0, max = length(DF$mz), style = 3)
+  Result <- vector(mode = "list", length = length(DF$My.mz))
+  pb <- txtProgressBar(min = 0, max = length(DF$My.mz), style = 3)
 
   for (i in 1:length(DF$mz)) {
     ## progress bar
     setTxtProgressBar(pb, i)
 
-    DB.list <- expand.grid.df(i, DF$mz[i], DF$rt[i], DB)
-    colnames(DB.list)[c(1:3)] <- c("QueryID", "My.mz", "My.RT")
+    DB.list <- expand.grid.df(i, DF[i, ], DB)
+    colnames(DB.list)[1] <- "QueryID"
     if(useRT == TRUE) {
-      if(!'rt' %in% colnames(DF)) {stop("Column 'rt' is not found in your data")}
-      if(!'rt' %in% colnames(DB)) {stop("Column 'rt' is not found in your database")}
+      if(!'My.RT' %in% colnames(DF)) {stop("Column 'rt' is not found in your data")}
+      if(!'DB.RT' %in% colnames(DB)) {stop("Column 'rt' is not found in your database")}
       ## filter according to ppm and RT
-      cal_ppm <- with(DB.list, (mz - My.mz) * 10^6 / mz)
+      cal_ppm <- with(DB.list, (DB.mz - My.mz) * 10^6 / DB.mz)
       cal_ppm <-  round(cal_ppm, digits = 2)
-      RT_diff <- with(DB.list, abs(My.RT - rt))
+      RT_diff <- with(DB.list, abs(My.RT - DB.RT))
       RT_diff <- round(RT_diff, digits = 2)
       DB.list <- cbind(DB.list, Cal.ppm = cal_ppm, RT.dif = RT_diff)
       Result[[i]] = DB.list[(abs(DB.list$Cal.ppm) <= ppm) & DB.list$RT.dif <= RT, ]
       row.names(Result[[i]]) <- NULL
     } else {
-      cal_ppm <- with(DB.list, (mz - My.mz) * 10^6 / mz)
+      cal_ppm <- with(DB.list, (DB.mz - My.mz) * 10^6 / DB.mz)
       cal_ppm <-  round(cal_ppm, digits = 2)
       DB.list <- cbind(DB.list, Cal.ppm = cal_ppm)
       Result[[i]] = DB.list[(abs(DB.list$Cal.ppm) <= ppm), ]
