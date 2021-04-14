0d44ef690ed3fb1957c115f2c73829bbf24febc1
YonghuiDong <yonghui.dong@gmail.com>
2021-04-14 13:55:11 +0300

    Update searchDB.R
    improve the speed

--- R/searchDB.R
+++ R/searchDB.R
@@ -26,7 +26,6 @@ searchDB <- function(DF, DB, ppm = 5, RT = 0.2, useRT = FALSE){
   cat("Searching started...\n")
 
   #(2) define function and search
-  expand.grid.df <- function(...) Reduce(function(...) merge(..., by = NULL), list(...))
   Result <- vector(mode = "list", length = length(DF$My.mz))
   pb <- txtProgressBar(min = 0, max = length(DF$My.mz), style = 3)
 
@@ -34,25 +33,29 @@ searchDB <- function(DF, DB, ppm = 5, RT = 0.2, useRT = FALSE){
     ## progress bar
     setTxtProgressBar(pb, i)
 
-    DB.list <- expand.grid.df(i, DF[i, ], DB)
-    colnames(DB.list)[1] <- "QueryID"
     if(useRT == TRUE) {
       if(!'My.RT' %in% colnames(DF)) {stop("Column 'rt' is not found in your data")}
       if(!'DB.RT' %in% colnames(DB)) {stop("Column 'rt' is not found in your database")}
       ## filter according to ppm and RT
-      cal_ppm <- with(DB.list, (DB.mz - My.mz) * 10^6 / DB.mz)
-      cal_ppm <-  round(cal_ppm, digits = 2)
-      RT_diff <- with(DB.list, abs(My.RT - DB.RT))
-      RT_diff <- round(RT_diff, digits = 2)
-      DB.list <- cbind(DB.list, Cal.ppm = cal_ppm, RT.dif = RT_diff)
-      Result[[i]] = DB.list[(abs(DB.list$Cal.ppm) <= ppm) & DB.list$RT.dif <= RT, ]
-      row.names(Result[[i]]) <- NULL
+      Result[[i]] <- subset(DB, abs(DB$DB.mz - DF$My.mz[i]) * 10^6 / DB$DB.mz <= ppm & abs(DB$DB.RT - DF$My.RT[i]) < RT)
+      if(dim(Result[[i]])[1] > 0) {
+        ppms <- (Result[[i]]$DB.mz - DF$My.mz[i]) * 10^6 / Result[[i]]$DB.mz
+        rts <- Result[[i]]$DB.RT - DF$My.RT[i]
+        Result[[i]] <- cbind(QuerryID = i, DF[i, ], Result[[i]], ppm = round(ppms, 2), RT_dif = round(rts, 2))
+        row.names(Result[[i]]) <- NULL
+      } else{
+        Result[[i]] <- NULL
+      }
+
     } else {
-      cal_ppm <- with(DB.list, (DB.mz - My.mz) * 10^6 / DB.mz)
-      cal_ppm <-  round(cal_ppm, digits = 2)
-      DB.list <- cbind(DB.list, Cal.ppm = cal_ppm)
-      Result[[i]] = DB.list[(abs(DB.list$Cal.ppm) <= ppm), ]
-      row.names(Result[[i]]) <- NULL
+      Result[[i]] <- subset(DB, abs(DB$DB.mz - DF$My.mz[i]) * 10^6 / DB$DB.mz <= ppm)
+      if(dim(Result[[i]])[1] > 0) {
+        ppms <- (Result[[i]]$DB.mz - DF$My.mz[i]) * 10^6 / Result[[i]]$DB.mz
+        Result[[i]] <- cbind(QuerryID = i, DF[i, ], Result[[i]], ppm = round(ppms, 2))
+        row.names(Result[[i]]) <- NULL
+      } else{
+        Result[[i]] <- NULL
+      }
     }
   }
 
